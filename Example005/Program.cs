c2a1b20d827c06d59a17f0b44402f7e945713cd5
KatelevskiyViktor <empirikk@ya.ru>
2022-06-05 18:12:11 +0300

    Add new dir, and create new program

---
+++ Example005/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Enter name user: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "masha")
+{
+    Console.WriteLine("Yeah, this is Masha!");
+}
+else
+{
+    Console.Write("Hi, ");
+    Console. WriteLine(username);
+}
+
