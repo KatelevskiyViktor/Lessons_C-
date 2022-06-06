47a2a88f9e40c35a0e09573fbd6a79dae99aed1b
KatelevskiyViktor <empirikk@ya.ru>
2022-06-06 19:16:48 +0300

    did home work

--- Seminars/s1/Program.cs
+++ Seminars/s1/Program.cs
@@ -1,1 +1,55 @@
-﻿
+﻿// Ex1:
+// Console.Write("Enter Num: ");
+// int n = int.Parse(Console.ReadLine());
+// Console.WriteLine("Num: " + n);
+// Console.WriteLine("Square: " + n * n);
+
+
+
+// Ex2:
+// Console.Write("Enter Num: ");
+// int a = int.Parse(Console.ReadLine());
+// Console.Write("Enter Square: ");
+// int b = int.Parse(Console.ReadLine());
+// 
+// if(b == a*a) Console.WriteLine("Yes " + b + " square " + a);
+// else Console.WriteLine("No " + b + " not a square " + a);
+
+
+
+// Ex3:
+// Console.Write("Enter Num: ");
+// int a = int.Parse(Console.ReadLine());
+// string[] day = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
+// 
+// Console.Write("You inter: " + day[a-1]);
+
+
+// Ex4:
+// Console.Write("Enter Num: ");
+// int a = int.Parse(Console.ReadLine());
+// int i = a;
+// int j = 1;
+// while(a >= 0){
+//     if(a != 0) {
+//         Console.Write("-" + a + ", ");
+//         a--;
+//     }else{
+//         Console.Write(a + ", ");
+//         a--;
+//         while(i != 0){
+//                 Console.Write(j + ", ");
+//                 i--;
+//                 j++;
+//    
+//         }
+//     }
+// }
+
+
+// Ex5
+// Console.Write("Enter Num: ");
+// string s = Console.ReadLine();
+// char[] sa = s.ToCharArray();
+// Console.Write(sa[sa.Length - 1]);
+
---
+++ Seminars/s1/hw/Program.cs
@@ -0,0 +1,40 @@
+﻿// Ex1
+// Console.Write("Enter first num: ");
+// int fn = int.Parse(Console.ReadLine());
+// Console.Write("Enter second num: ");
+// int sn = int.Parse(Console.ReadLine());
+// if(fn > sn) Console.Write(fn + " > " + sn);
+// else Console.Write(fn + " < " + sn);
+
+
+
+// Ex2
+// Console.Write("Enter first num: ");
+// int fn = int.Parse(Console.ReadLine());
+// Console.Write("Enter second num: ");
+// int sn = int.Parse(Console.ReadLine());
+// Console.Write("Enter thid num: ");
+// int thn = int.Parse(Console.ReadLine());
+// if(fn > sn && fn > thn) Console.Write("Max val: " + fn);
+// else if(sn > fn && sn > thn) Console.Write("Max val: " + sn);
+// else Console.Write("Max val: " + thn);
+
+
+// Ex3
+// Console.Write("Enter num: ");
+// int n = int.Parse(Console.ReadLine());
+// if(n % 2 == 0) Console.Write(n + " - even num!");
+// else Console.Write(n + " - odd num!");
+
+
+
+// Ex4
+Console.Write("Enter num: ");
+int n = int.Parse(Console.ReadLine());
+int i = 1;
+while(i <= n){
+    if(i % 2 == 0) Console.Write(i + ", ");
+    i++;
+}
+
+
