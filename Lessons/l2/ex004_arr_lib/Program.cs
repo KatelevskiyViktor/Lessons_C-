48cb117275d4650e7b1206c4103811313f55b231
KatelevskiyViktor <empirikk@ya.ru>
2022-06-08 12:24:10 +0300

    Add new dir and create new ex

---
+++ Lessons/l2/ex004_arr_lib/Program.cs
@@ -0,0 +1,40 @@
+﻿void FillArray(int[] collection){
+    int length = collection.Length;
+    int i = 0;
+    while(i < length){
+        collection[i] = new Random().Next(1, 10);
+        i++;
+    }
+}
+
+void PrintArr(int[] col){
+    int count = col.Length;
+    int position = 0;
+    while(position < count){
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+int IndexOf(int[] collection, int find){
+    int count = collection.Length;
+    int index = 0;
+    int position = -1;
+
+    while(index < count){
+        if(collection[index] == find){
+            position = index;
+            break;
+        index++;
+    }
+    return position;
+}
+
+int[] array = new int[10];
+
+FillArray(array);
+PrintArr(array);
+Console.WriteLine();
+
+int pos = IndexOf(array, 4);
+Console.WriteLine(pos);
