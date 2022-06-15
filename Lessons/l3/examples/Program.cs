4e90532cc2c33582c7c50ffbc9b54ec32fd56d2e
KatelevskiyViktor <empirikk@ya.ru>
2022-06-15 23:07:17 +0300

    Add new dir for lesson 3,  made new example to this lesson

---
+++ Lessons/l3/examples/Program.cs
@@ -0,0 +1,172 @@
+﻿// Ex1
+// void Method1(){
+//     Console.WriteLine("Author ...");
+// }
+// 
+// Method1();
+
+
+// Ex2:
+// void Method2(string str){
+//     Console.WriteLine(str);
+// }
+// 
+// Method2("Hi!");
+
+
+// Ex3:
+// void Method3(string str, int count){
+//     int i = 0;
+//     while(i < count){
+//         Console.WriteLine(str);
+//         i++;
+//     }
+// }
+// 
+// Method3("Hi!", 4);
+
+
+// Ex4:
+// int Method4(){
+//     return DateTime.Now.Year;
+// }
+// 
+// int year = Method4();
+// Console.WriteLine(year);
+
+
+// Ex5:
+// string Method5(int count, string str){
+// 
+//     int i = 0;
+//     string result = String.Empty;
+//     while(i < count){
+//         result += str;
+//         i++;
+//     }
+//     return result;
+// }
+// 
+// string res = Method5(10, "Hi! ");
+// Console.WriteLine(res);
+
+
+// Ex6:
+// string Method6(int count, string str){
+// 
+//     string result = String.Empty;
+// 
+//     for(int i = 0;i < count; i++){
+//         result += str;
+//     }
+// 
+//     return result;
+// }
+// 
+// string res = Method6(10, "Hi! ");
+// Console.WriteLine(res);
+
+
+// ex7:
+// void Method7(){
+// 
+//     for(int i = 2; i <= 10; i++){
+//         for(int j = 2; j <= 10; j++){
+//             Console.WriteLine($"{i} x {j} = {i * j}");
+//         }
+//         Console.WriteLine();
+//     }
+// 
+// }
+// 
+// Method7();
+
+
+// ex8:
+// string str = "Идейные соображения - высшего порядка, а также дальнейшее"
+// + " развитие различных форм деятельности требуют  - от нас анализа дальнейших"
+// + " направлений - развития. Идейные соображения высшего порядка, а также" 
+// + " укрепление и развитие структуры требуют определения и уточнения "
+// + "направлений прогрессивного развития.";
+// 
+// string Replace(string str, char oldValue, char newValue){
+// 
+//     string result = String.Empty;
+// 
+//     int length = str.Length;
+// 
+//     for(int i = 0; i < length; i++)
+//     {
+//         if(str[i] == oldValue) result += $"{newValue}";
+//         else result += $"{str[i]}";
+//     }
+// 
+//     return result;
+// 
+// }
+// 
+// string newText = Replace(str, ' ', '|');
+// Console.WriteLine(newText);
+
+
+// ex9:
+// string str = "Идейные соображения - высшего порядка, а также дальнейшее"
+// + " развитие различных форм деятельности требуют  - от нас анализа дальнейших"
+// + " направлений - развития. Идейные соображения высшего порядка, а также" 
+// + " укрепление и развитие структуры требуют определения и уточнения "
+// + "направлений прогрессивного развития.";
+// 
+// string Replace(string str, char oldValue, char newValue){
+// 
+//     string result = String.Empty;
+// 
+//     int length = str.Length;
+// 
+//     for(int i = 0; i < length; i++)
+//     {
+//         if(str[i] == oldValue) result += $"{newValue}";
+//         else result += $"{str[i]}";
+//     }
+// 
+//     return result;
+// 
+// }
+// 
+// string newText = Replace(str, ' ', '|');
+// Console.WriteLine(newText);
+
+
+// ex10:
+int[] arr = {1, 5, 4, 3, 2, 6, 7, 1, 1};
+
+void PrintArr(int[] arr){
+
+    int count = arr.Length;
+
+    for(int i = 0; i < count; i++){
+        Console.Write($"{arr[i]}");
+    }
+    Console.WriteLine();
+}
+
+void SelectionSort(int[] arr){
+    for (int i = 0; i < arr.Length; i++)
+    {
+       int minPosition = i;
+
+        for (int j = i + 1; j < arr.Length; j++)
+        {
+            if(arr[j] < arr[minPosition])  minPosition = j;
+        }
+
+       int temp = arr[i];
+       arr[i] = arr[minPosition];
+       arr[minPosition] = temp;
+    }
+}
+
+PrintArr(arr);
+
+SelectionSort(arr);
+
+PrintArr(arr);
---
+++ Seminars/s3/Program.cs
@@ -0,0 +1,82 @@
+﻿// Ex1
+// Console.WriteLine("Enter X: ");
+// int x = int.Parse(Console.ReadLine());
+// 
+// Console.WriteLine("Enter Y: ");
+// int y = int.Parse(Console.ReadLine());
+// 
+// Console.WriteLine($"Coordinates X:{x}, Y:{y}");
+// 
+// if (x > 0 && y > 0)
+// {
+//     Console.WriteLine(1);
+// }
+// else if(x < 0 && y > 0)
+// {
+//     Console.WriteLine(2);
+// }
+// else if(x < 0 && y < 0)
+// {
+//     Console.WriteLine(3);
+// }
+// else if(x > 0 && y < 0)
+// {
+//     Console.WriteLine(4);
+// }
+// else{
+//     Console.WriteLine("Coordinates not work");
+// }
+
+
+
+// Ex2
+// Console.WriteLine("Enter num quarter: ");
+// int nq = int.Parse(Console.ReadLine());
+
+// if (nq == 1)
+// {
+//     Console.WriteLine("Coordinates from X:(0:+&) Y:(0:+&)");
+// }
+// else if(nq == 2)
+// {
+//     Console.WriteLine("Coordinates from X:(0:-&) Y:(0:+&)");
+// }
+// else if(nq == 3)
+// {
+//    Console.WriteLine("Coordinates from X:(0:-&) Y:(0:-&)");
+// }
+// else if(nq == 4)
+// {
+//     Console.WriteLine("Coordinates from X:(0:+&) Y:(0:-&)");
+// }
+// else{
+//     Console.WriteLine("wrong num!");
+// }
+
+
+// Ex3:
+// Console.WriteLine("Enter num x1: ");
+// int x1 = int.Parse(Console.ReadLine());
+//
+// Console.WriteLine("Enter num x2: ");
+// int x2 = int.Parse(Console.ReadLine());
+//
+// Console.WriteLine("Enter num y1: ");
+// int y1 = int.Parse(Console.ReadLine());
+//
+// Console.WriteLine("Enter num y2: ");
+// int y2 = int.Parse(Console.ReadLine());
+//
+// Console.WriteLine(Math.Round(Math.Sqrt(Math.Pow(x2-x1,2) + Math.Pow(y2-y1,2)), 3));
+
+
+// Ex4:
+//  Console.Write("Enter num: ");
+// int n = int.Parse(Console.ReadLine());
+// int count = 1;
+// 
+// while(count <= n){
+//     Console.Write(Math.Pow(count, 2) + ", ");
+//     count++;
+// }
+
